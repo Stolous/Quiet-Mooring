45f51a444f820f8752b859032dd104ca590fcb3b
Stolous <stolous@gmail.com>
2013-12-19 17:00:05 +0100

    finished option panel and adding activation panel when you touch the power button

---
+++ Quiet Mooring/ActivationViewController.h
@@ -0,0 +1,13 @@
+//
+//  ActivationViewController.h
+//  Quiet Mooring
+//
+//  Created by Véré Baptiste on 19/12/2013.
+//  Copyright (c) 2013 Magic Compagny. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ActivationViewController : NSObject
+
+@end
---
+++ Quiet Mooring/ActivationViewController.m
@@ -0,0 +1,13 @@
+//
+//  ActivationViewController.m
+//  Quiet Mooring
+//
+//  Created by Véré Baptiste on 19/12/2013.
+//  Copyright (c) 2013 Magic Compagny. All rights reserved.
+//
+
+#import "ActivationViewController.h"
+
+@implementation ActivationViewController
+
+@end
