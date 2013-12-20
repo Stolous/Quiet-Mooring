4616072226a144ea5e41590f94ff5fdec7b94338
Stolous <stolous@gmail.com>
2013-12-20 15:08:19 +0100

    adding Activation panel (debug)

---
+++ Quiet Mooring/activationSegue.h
@@ -0,0 +1,13 @@
+//
+//  activationSegue.h
+//  Quiet Mooring
+//
+//  Created by Véré Baptiste on 20/12/2013.
+//  Copyright (c) 2013 Magic Compagny. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface activationSegue : NSObject
+
+@end
---
+++ Quiet Mooring/activationSegue.m
@@ -0,0 +1,13 @@
+//
+//  activationSegue.m
+//  Quiet Mooring
+//
+//  Created by Véré Baptiste on 20/12/2013.
+//  Copyright (c) 2013 Magic Compagny. All rights reserved.
+//
+
+#import "activationSegue.h"
+
+@implementation activationSegue
+
+@end
